9e4fe81da0250d1e862f6c50ae9be135656b5ddb
Saad Ahmed <dropheaven@live.com>
2019-06-16 20:36:16 -0400

    passing expense data to ExpenseForm in props

--- client/src/components/EditExpense.js
+++ client/src/components/EditExpense.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
+import ExpenseForm from "./ExpenseForm";
 
-const EditExpense = () => (
-  <div>
-    <p>Edit expense</p>
-  </div>
-);
+const EditExpense = props => {
+  return (
+    <div className="container text-center" id="expense-form">
+      <h1 className="text-center mb-3">Edit Expense</h1>
+      <ExpenseForm expense={props.expense} onSubmit={expense => {}} />
+    </div>
+  );
+};
 
 const mapStateToProps = (state, props) => {
   return {
     expense: state.expenses.find(
-      expense => expense.id === props.match.params.id
+      expense => expense.id === parseInt(props.match.params.id)
     )
   };
 };

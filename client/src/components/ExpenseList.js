681479ff423e9eb1a0ce0d75145eb3ccece8bdc5
Saad Ahmed <dropheaven@live.com>
2019-06-16 21:48:16 -0400

    testing out fetch call to rails server

--- client/src/components/ExpenseList.js
+++ client/src/components/ExpenseList.js
@@ -2,6 +2,11 @@ import React from "react";
 import { connect } from "react-redux";
 import Expense from "./Expense";
 
+const API_URL = process.env.REACT_APP_API_URL;
+fetch(`${API_URL}/expenses`)
+  .then(response => response.json())
+  .then(expenses => console.log(expenses));
+
 const ExpenseList = props => (
   <React.Fragment>
     {props.expenses.map(expense => (

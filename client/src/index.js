e6d217e09174ab1faf963e72e7a9eb65ce935c36
Saad Ahmed <dropheaven@live.com>
2019-06-16 21:47:18 -0400

    no longer adding expenses to store via dispatch

--- client/src/index.js
+++ client/src/index.js
@@ -8,44 +8,44 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./styles/main.css";
 
 const store = configureStore();
-store.dispatch(
-  addExpense({
-    id: 1,
-    category: "utilities",
-    name: "Water bill",
-    dueDate: "May 15, 2019",
-    amount: 2000
-  })
-);
+// store.dispatch(
+//   addExpense({
+//     id: 1,
+//     category: "utilities",
+//     name: "Water bill",
+//     dueDate: "May 15, 2019",
+//     amount: 2000
+//   })
+// );
 
-store.dispatch(
-  addExpense({
-    id: 2,
-    category: "utilities",
-    name: "Verizon Internet",
-    dueDate: "July 01, 2019",
-    amount: 8000
-  })
-);
+// store.dispatch(
+//   addExpense({
+//     id: 2,
+//     category: "utilities",
+//     name: "Verizon Internet",
+//     dueDate: "July 01, 2019",
+//     amount: 8000
+//   })
+// );
 
-store.dispatch(
-  addExpense({
-    id: 3,
-    category: "health",
-    name: "Gym",
-    dueDate: "August 21, 2019",
-    amount: 2500
-  })
-);
-store.dispatch(
-  addExpense({
-    id: 4,
-    category: "credit cards",
-    name: "Citi",
-    dueDate: "June 09, 2019",
-    amount: 15000
-  })
-);
+// store.dispatch(
+//   addExpense({
+//     id: 3,
+//     category: "health",
+//     name: "Gym",
+//     dueDate: "August 21, 2019",
+//     amount: 2500
+//   })
+// );
+// store.dispatch(
+//   addExpense({
+//     id: 4,
+//     category: "credit cards",
+//     name: "Citi",
+//     dueDate: "June 09, 2019",
+//     amount: 15000
+//   })
+// );
 
 const jsx = (
   <Provider store={store}>

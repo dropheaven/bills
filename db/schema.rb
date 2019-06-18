92808f939af3621219a218a26c67f0bec6734518
Saad Ahmed <dropheaven@live.com>
2019-06-17 21:56:05 -0400

    add user model & user_index on expenses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_005209) do
+ActiveRecord::Schema.define(version: 2019_06_17_031916) do
 
   create_table "expenses", force: :cascade do |t|
     t.string "name"
     t.string "amount"
     t.date "due_date"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end

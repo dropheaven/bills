28356e846665b18862b8de7f8dd5497847160eba
Saad Ahmed <dropheaven@live.com>
2019-06-18 08:54:42 -0400

    import thunk, middleware, compose and user reducer

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,11 +1,19 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import usersReducer from "../reducers/users";
 import expensesReducer from "../reducers/expenses";
 
 export default () => {
+  const composeEnhancer =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
-    combineReducers({
-      expenses: expensesReducer
-    })
+    combineReducers(
+      {
+        users: usersReducer,
+        expenses: expensesReducer
+      },
+      composeEnhancer(applyMiddleware(thunk))
+    )
   );
 
   return store;

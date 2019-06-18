596d9456b05452f69a93a9a26c67f41d2de7907e
Saad Ahmed <dropheaven@live.com>
2019-06-17 21:56:45 -0400

    seed database with a user and a some expenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,7 @@
-# Expenses
-Expense.destroy_all
+User.destroy_all
 
-Expense.create!([
-  {
-    name: 'Water bill',
-    amount: 40,
-    due_date: 'August 5, 2019'
-  },
-  {
-    name: 'Electricity',
-    amount: 80,
-    due_date: 'July 2, 2019'
-  },
-  {
-    name: 'Phone',
-    amount: 60,
-    due_date: 'June 28, 2019'
-  },
-  {
-    name: 'Rent',
-    amount: 1600,
-    due_date: 'September 15, 2019'
-  },
-  {
-    name: 'Credit card',
-    amount: 204,
-    due_date: 'April 21, 2019'
-  }
-])
+first_user = User.create(name: "first user", email: "first@email.com", password: "password")
+first_user.expenses.create( name: 'Electricity', amount: 80, due_date: 'July 2, 2019')
+first_user.expenses.create( name: 'Phone', amount: 60, due_date: 'June 28, 2019')
+first_user.expenses.create(name: 'Rent', amount: 1600, due_date: 'September 15, 2019')
+first_user.expenses.create(name: 'Credit card', amount: 204, due_date: 'April 21, 2019')
